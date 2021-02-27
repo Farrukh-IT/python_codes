8598ba77525a595011dca63346ffc5c57a384bf5
Smaylov Farrukh <78921938+Farrukh-IT@users.noreply.github.com>
2021-02-27 21:39:41 +0000

    Create abc.py

---
+++ README.md/abc.py
@@ -0,0 +1,13 @@
+# python_codes
+
+boshlanish = input("Boshlanishini kiritng: ")
+oxiri = input("Oxirini kiritng: ")
+
+while boshlanish <= oxiri:
+    print(boshlanish, end= ' ')
+    boshlanish = chr(ord(boshlanish)+1)
+
+print()
+
+
+HARIFLARNI ORSIDAGI HARIFLARNI CHIQARIB BERUVCH DASTUR🔠🔚
