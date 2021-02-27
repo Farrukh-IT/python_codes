b044bc79c4cade988183253558327d63a1006e44
Smaylov Farrukh <78921938+Farrukh-IT@users.noreply.github.com>
2021-02-27 21:35:48 +0000

    Create uch.py

---
+++ README.md/uch.py
@@ -0,0 +1,17 @@
+# python_codes
+num = int(input("Qanch satrli uchbuchak burchak kerak:  "))
+for i in range(1,num +1):
+    print(" " *(num -i)+"* "*i)
+
+
+
+         * 
+        * * 
+       * * * 
+      * * * * 
+     * * * * * 
+    * * * * * * 
+   * * * * * * * 
+  * * * * * * * * 
+ * * * * * * * * * 
+* * * * * * * * * * 
